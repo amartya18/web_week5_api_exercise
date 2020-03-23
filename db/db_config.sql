6b5b314daeec7887d2924c0ca64a4350ddc84974
amartya <amartya.sk@gmail.com>
2020-03-23 18:23:42 +0700

    added sql dump

---
+++ db/db_config.sql
@@ -0,0 +1,53 @@
+-- MariaDB dump 10.17  Distrib 10.4.10-MariaDB, for osx10.15 (x86_64)
+--
+-- Host: localhost    Database: web_week5_php_api
+-- ------------------------------------------------------
+-- Server version	10.4.10-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `no` int(11) NOT NULL AUTO_INCREMENT,
+  `studentid` varchar(12) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'1122334455','Tim Burton','e10adc3949ba59abbe56e057f20f883e'),(2,'123456789','Alice Wonderland','e10adc3949ba59abbe56e057f20f883e'),(12,'2201841212','Geo Hotz','$2y$10$.QDjBiQYLm5X1vY5nxxUIOgq2.72XPAmqAK4NGShGQYu6cN5hM7LC'),(13,'220132153','Mark Zuckeberg','$2y$10$qGsOIcjugmOpld2NMlxW.Orr7twu0vY3yWdM120Y7zClarxgNej3C'),(15,'2201847843','Sundar P','$2y$10$BeG2OafpEObG.kN8QM5ApOG0gW78hUAtj1B.Q1V//9GFjLZ9e6EUS'),(16,'220132432','Larry Page','$2y$10$Uwx5U0aFOJO1snKV2YfPCuQRufJdbGWN1x26BA12/Xqdaa6btFxDW');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-23 18:22:35
